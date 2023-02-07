42a81efb5fe0d61daddcf1f18f20363bf425430e
Khai Tran <112424169+Khai-Minh-Tran@users.noreply.github.com>
2023-02-07 16:19:40 -0500

    Add files via upload

---
+++ Khaitran/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Deliverable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution(1): Hello, World!");
+            string stg = "Hello, World!";
+            Console.WriteLine("Solution(2): " + stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + UserName);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + age);
+        }
+    }
+}
